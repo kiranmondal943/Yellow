0ae9af9a3b3992ff08119f25b0ddae84f9da0e0b
kiranmondal943 <76810247+kiranmondal943@users.noreply.github.com>
2026-03-17 14:32:16 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://www.yellowpages.uz/en/search/"
+QUERY = "dental"
+PAGES_TO_SCRAPE = 5   # Change as needed
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+}
